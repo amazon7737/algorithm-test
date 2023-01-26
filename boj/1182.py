421ccf0769df2fd7971c0b528f863f0c992a5fb1
amazon7737 <76634341+amazon7737@users.noreply.github.com>
2023-01-26 15:11:03 +0900

    Update and rename 1182_2003.py to 1182.py

--- boj/1182.py
+++ boj/1182.py
@@ -18,24 +18,6 @@ dfs(0, 0)
 print(cnt)
 
 
-#2003
 
-import sys
-
-n, m = map(int, sys.stdin.readline().split())
-arr = list(map(int , sys.stdin.readline().split()))
-
-cnt = 0
-for i in range(n):
-  tmp = 0
-  for h in range(i, n):
-    tmp += arr[h]
-    if tmp == m:
-      cnt += 1
-      break
-    elif tmp > m:
-      break
-
-print(cnt)
 
 
