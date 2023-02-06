d2fe165dcd9601827ba78bedbfe7fb579ab9ece0
amazon7737 <76634341+amazon7737@users.noreply.github.com>
2023-02-06 13:52:49 +0900

    New

--- programmers/고득점/정렬/가장큰수.py
+++ programmers/고득점/정렬/가장큰수.py
@@ -4,7 +4,6 @@ def solution(numbers):
     a_list = list(map(str, numbers))
     
     a_list.sort(key = lambda x:x*3, reverse = True)
-    print(a_list)
     answer = ''
     answer = answer.join(a_list)
-    print(answer)
+    return str(int(answer))
