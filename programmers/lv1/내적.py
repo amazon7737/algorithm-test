149530b38bbe2dca820b4c06df9cc435b53ddaeb
amazon7737 <76634341+amazon7737@users.noreply.github.com>
2023-07-14 15:08:51 +0900

    Delete programmers directory

--- programmers/lv1/내적.py
+++
@@ -1,5 +0,0 @@
-def solution(a, b):
-    c = 0
-    for i in range(len(a)):
-        c += a[i]*b[i]
-    return c    
--- programmers/lv1/서울에서김서방찾기.py
+++
@@ -1,5 +0,0 @@
-def solution(seoul):
-    for i in range(len(seoul)):
-        if seoul[i] == "Kim":
-            a = seoul.index(seoul[i])
-    return "김서방은"+" "+str(a)+"에 있다"
