3722b35fa5a2c6ad938ec9f70d82ebf82eb383f5
김강민 <amazon7737@gmail.com>
2023-08-30 14:39:33 +0900

    new

--- problem_solving/programmers/lv1/두정수사이의합.py
+++ problem_solving/programmers/lv1/두정수사이의합.py
@@ -7,3 +7,10 @@ def solution(a, b):
         for i in range(a, b-1, -1):
             arr.append(i)
     return sum(arr)
+
+def solution(a, b):
+    if a < b :
+        return sum([i for i in range(a, b+1)])
+    elif a > b:
+        return sum([i for i in range(b, a+1)])
+    return a
